dcb0691bd94ea93508d30a120d30aa91faa8015c
David Siegers <siegersdavid@googlemail.com>
2015-01-12 17:52:30 +0100

    added some files

---
+++ snipcol_0001.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siegersdavid
+ * Date: 12/01/15
+ * Time: 17:24
+ */
+
+
+function fcConcatStrings($paraStrA, $paraStrB) {
+
+    $returnStr = $paraStrA.$paraStrB;
+
+    return $returnStr;
+
+}
+
+echo fcConcatStrings('tree','house');
+
+
+
